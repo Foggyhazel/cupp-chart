f61fc4ee4cf4f46c81f86ac04aa2e90793fa7a16
chaipat_p <chaipat_p@live.com>
2020-05-12 20:57:40 +0700

    add annotation

--- src/TouchHandler.jsx
+++ src/TouchHandler.jsx
@@ -1,16 +1,30 @@
 import React, { createContext, useState, useMemo, useContext } from "react";
 
-const HandlerContext = createContext();
-const LocationContext = createContext();
+const HandlerContext = createContext({});
+const LocationContext = createContext({ x: 0, y: 0, active: false });
 
+/**
+ * Return event handlers for component.
+ * Use inside 'TouchHandler'
+ * @returns {{onTouchStart: function, onTouchMove: function, onTouchEnd: function}}
+ */
 export const useTouchHandlers = () => {
   return useContext(HandlerContext);
 };
 
+/**
+ * Use inside TouchHandler context.
+ * Return touch location inside element that generate touch event
+ * @return {{x: number, y:number, active: boolean}}
+ */
 export const useTouchLocation = () => {
   return useContext(LocationContext);
 };
 
+/**
+ * Provide touch handler to be bind to component and store touch event info.
+ * Use together with 'useTouchLocation' and 'useTouchHandlers'
+ */
 export default function TouchHandler({ children }) {
   const [loc, setLoc] = useState({ x: 0, y: 0, active: false });
   const handlers = useMemo(
